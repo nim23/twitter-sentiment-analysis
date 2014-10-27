e2c8e36732d37cd47a6ef9cf466e6016633f8796
nim23 <n.gurung-10@student.lboro.ac.uk>
2014-10-27 20:53:19 +0000

    Webpack updated file.

--- app/dist/js/1.main.js
+++ app/dist/js/1.main.js
@@ -1,97 +1,17 @@
-webpackJsonp([1],{
-
-/***/ 8:
-/***/ function(module, exports, __webpack_require__) {
-
-	/** @jsx React.DOM */
-	'use strict';
-	var React = __webpack_require__(6);
-	var Avatar = __webpack_require__(155);
-
-	module.exports = React.createClass({displayName: 'exports',
-	  render: function(){
-	    return (
-	        React.DOM.div(null, 
-	          Avatar({imgUrl: this.props.twit.profile_image_url}), 
-	          this.props.twit.text
-	        )
-	    );
-	  }
-	});
-
-
-/***/ },
-
-/***/ 155:
-/***/ function(module, exports, __webpack_require__) {
-
-	/** @jsx React.DOM */
-	'use strict';
-	var React = __webpack_require__(6);
-
-	module.exports = React.createClass({displayName: 'exports',
-	  render: function(){
-	    return (React.DOM.span(null, 
-	              React.DOM.img({src: this.props.imgUrl})
-	            ));
-	  }
-	});
-
-
-/***/ },
-
-/***/ 157:
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-	var Backbone = __webpack_require__(2);
-	var Twit = __webpack_require__(158);
-	module.exports = Backbone.Collection.extend({
-	  model: Twit
-	});
-
-
-/***/ },
-
-/***/ 158:
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-	var Backbone = __webpack_require__(2);
-	module.exports = Backbone.Model.extend({});
-
-
-/***/ },
-
-/***/ 159:
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-	module.exports = {
-		componentDidMount: function(){
-				this._boundForceUpdate = this.forceUpdate.bind(this, null);
-				this.getBackboneObject().on('all', this._boundForceUpdate, this);
-			},
-		componentWillUnmount: function(){
-				this.getBackboneObject().off('all', this._boundForceUpdate);
-			},
-		getBackboneObject: function(){
-				return this.props.collection || this.props.model;
-			}
-	};
-
-
-/***/ },
-
-/***/ 160:
+webpackJsonp([1],[
+/* 0 */,
+/* 1 */,
+/* 2 */,
+/* 3 */,
+/* 4 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/** @jsx React.DOM */
 	'use strict';
 	var Backbone = __webpack_require__(2);
-	var Twits = __webpack_require__(157);
-	var Twit = __webpack_require__(158);
-	var twitsApp = __webpack_require__(161);
+	var Twits = __webpack_require__(10);
+	var Twit = __webpack_require__(11);
+	var twitsApp = __webpack_require__(8);
 	var React = __webpack_require__(6);
 
 	module.exports = Backbone.View.extend({
@@ -117,15 +37,17 @@ webpackJsonp([1],{
 
 
 /***/ },
-
-/***/ 161:
+/* 5 */,
+/* 6 */,
+/* 7 */,
+/* 8 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/** @jsx React.DOM */
 	'use strict';
 	var React = __webpack_require__(6);
-	var Twit = __webpack_require__(8);
-	var BackboneMixin = __webpack_require__(159);
+	var Twit = __webpack_require__(15);
+	var BackboneMixin = __webpack_require__(16);
 
 	module.exports = React.createClass({displayName: 'exports',
 	  mixins: [BackboneMixin],
@@ -142,6 +64,87 @@ webpackJsonp([1],{
 	});
 
 
-/***/ }
+/***/ },
+/* 9 */,
+/* 10 */
+/***/ function(module, exports, __webpack_require__) {
 
-});
+	'use strict';
+	var Backbone = __webpack_require__(2);
+	var Twit = __webpack_require__(11);
+	module.exports = Backbone.Collection.extend({
+	  model: Twit
+	});
+
+
+/***/ },
+/* 11 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	var Backbone = __webpack_require__(2);
+	module.exports = Backbone.Model.extend({});
+
+
+/***/ },
+/* 12 */,
+/* 13 */,
+/* 14 */,
+/* 15 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/** @jsx React.DOM */
+	'use strict';
+	var React = __webpack_require__(6);
+	var Avatar = __webpack_require__(17);
+
+	module.exports = React.createClass({displayName: 'exports',
+	  render: function(){
+	    return (
+	        React.DOM.div(null, 
+	          Avatar({imgUrl: this.props.twit.profile_image_url}), 
+	          this.props.twit.text
+	        )
+	    );
+	  }
+	});
+
+
+/***/ },
+/* 16 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	module.exports = {
+		componentDidMount: function(){
+				this._boundForceUpdate = this.forceUpdate.bind(this, null);
+				this.getBackboneObject().on('all', this._boundForceUpdate, this);
+			},
+		componentWillUnmount: function(){
+				this.getBackboneObject().off('all', this._boundForceUpdate);
+			},
+		getBackboneObject: function(){
+				return this.props.collection || this.props.model;
+			}
+	};
+
+
+/***/ },
+/* 17 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/** @jsx React.DOM */
+	'use strict';
+	var React = __webpack_require__(6);
+
+	module.exports = React.createClass({displayName: 'exports',
+	  render: function(){
+	    return (React.DOM.span(null, 
+	              React.DOM.img({src: this.props.imgUrl})
+	            ));
+	  }
+	});
+
+
+/***/ }
+]);
